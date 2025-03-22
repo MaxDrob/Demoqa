a1e69cae034c24d689046b6c041f08573fc9f9b1
Max Drob <maxdrobs@gmail.com>
2025-03-23 01:44:28 +0300

    first_commit

---
+++ tests/test_check_icon_demoqa.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_check_icon():
+    driver = webdriver.Chrome()
+    driver.get('https://demoqa.com/')
+
+    icon = driver.find_element(By.CSS_SELECTOR, '#app > header > a')
+
+    if icon is not None:
+        print('Found')
+    else:
+        print('Not found')
